0f1bfcce30cb464aa39bf85f2a90ec0f01cd12ce
SSZA <angel.sanzam@educa.jcyl.es>
2026-03-05 17:48:39 +0100

    comienzo

---
+++ src/domain/Character.java
@@ -0,0 +1,13 @@
+package domain;
+
+public class Character {
+    private String id;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+}
